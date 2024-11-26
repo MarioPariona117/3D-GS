5d06c045f31f5257570cea3700a1a1dd851ff4b6
elacs <keeyunshao@gmail.com>
2024-11-26 17:58:55 +0000

    print some stats

--- train.py
+++ train.py
@@ -169,6 +169,11 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
                 if iteration > opt.densify_from_iter and iteration % opt.densification_interval == 0:
                     size_threshold = 20 if iteration > opt.opacity_reset_interval else None
                     gaussians.densify_and_prune(opt.densify_grad_threshold, 0.005, scene.cameras_extent, size_threshold, radii)
+                    print('growth_directions_probablities:')
+                    print(gaussians.growth_directions_probabilities)
+                    print('growth_length_s:')
+                    print(gaussians.growth_length_s)
+                    print('\n')
                 
                 if iteration % opt.opacity_reset_interval == 0 or (dataset.white_background and iteration == opt.densify_from_iter):
                     gaussians.reset_opacity()
