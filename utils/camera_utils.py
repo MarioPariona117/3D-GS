235d78b7f38017f21aad5f0159b9af830e6430ec
Mario Pariona Molocho <mariopariona117@gmail.com>
2024-12-03 15:07:21 +0000

    .

--- utils/camera_utils.py
+++ utils/camera_utils.py
@@ -38,7 +38,7 @@ def loadCam(args, id, cam_info, resolution_scale, is_nerf_synthetic, is_test_dat
     else:
         invdepthmap = None
         
-    print(f"Loading camera {cam_info.depth_path}")
+    # print(f"Loading camera {cam_info.depth_path}")
     orig_w, orig_h = image.size
     if args.resolution in [1, 2, 4, 8]:
         resolution = round(orig_w/(resolution_scale * args.resolution)), round(orig_h/(resolution_scale * args.resolution))
