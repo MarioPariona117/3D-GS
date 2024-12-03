ad7ab18fa7001ffc67aa34fe1bf3009b87b9c986
Ryuichi-Student <rt590@cam.ac.uk>
2024-12-03 16:30:59 +0000

    COLAB: Working speedup, testing performance fix with different strategy

--- train.py
+++ train.py
@@ -143,21 +143,6 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
 
         iter_end.record()
 
-        if iteration < opt.densify_until_iter:
-            gaussians.max_radii2D[visibility_filter] = torch.max(gaussians.max_radii2D[visibility_filter], radii[visibility_filter])
-            gaussians.add_densification_stats(viewspace_point_tensor, visibility_filter)
-            if iteration > opt.densify_from_iter and iteration % opt.densification_interval == 0:
-                size_threshold = 20 if iteration > opt.opacity_reset_interval else None
-                gaussians.densify_and_prune(
-                    opt.densify_grad_threshold, 0.005, scene.cameras_extent, size_threshold, None, differentiable=True
-                )
-            with torch.no_grad():
-                if iteration > opt.densify_from_iter and iteration % opt.densification_interval == 0:
-                    size_threshold = 20 if iteration > opt.opacity_reset_interval else None
-                    gaussians.densify_and_prune(opt.densify_grad_threshold, 0.005, scene.cameras_extent, size_threshold, radii, differentiable=False)
-                if iteration % opt.opacity_reset_interval == 0 or (dataset.white_background and iteration == opt.densify_from_iter):
-                    gaussians.reset_opacity()
-
         with torch.no_grad():
             # Progress bar
             ema_loss_for_log = 0.4 * loss.item() + 0.6 * ema_loss_for_log
@@ -174,6 +159,15 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
             if (iteration in saving_iterations):
                 print("\n[ITER {}] Saving Gaussians".format(iteration))
                 scene.save(iteration)
+
+            if iteration < opt.densify_until_iter:
+                gaussians.max_radii2D[visibility_filter] = torch.max(gaussians.max_radii2D[visibility_filter], radii[visibility_filter])
+                gaussians.add_densification_stats(viewspace_point_tensor, visibility_filter)
+                if iteration > opt.densify_from_iter and iteration % opt.densification_interval == 0:
+                    size_threshold = 20 if iteration > opt.opacity_reset_interval else None
+                    gaussians.densify_and_prune(opt.densify_grad_threshold, 0.005, scene.cameras_extent, size_threshold, radii)
+                if iteration % opt.opacity_reset_interval == 0 or (dataset.white_background and iteration == opt.densify_from_iter):
+                    gaussians.reset_opacity()
                 
             # Optimizer step
             if iteration < opt.iterations:
