da141529771a27df53d75c0279b0f78d50908124
elacs <keeyunshao@gmail.com>
2024-12-04 18:33:04 +0000

    update just cloned mask after split

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -544,6 +544,8 @@ class GaussianModel:
 
         new_newly_cloned = torch.zeros(new_rotation.size()[0], device = "cuda", dtype = torch.bool)
 
+        self.just_cloned_mask = torch.cat((self.just_cloned_mask, torch.zeros(new_rotation.size()[0], device = "cuda", dtype = torch.bool)))
+
         self.densification_postfix(new_xyz, new_features_dc, new_features_rest, new_opacity, new_scaling, new_rotation, new_tmp_radii, new_s_prime, new_v, new_growth_directions_probabilities, new_growth_length_s, new_newly_cloned)
 
         prune_filter = torch.cat((selected_pts_mask, torch.zeros(N * selected_pts_mask.sum(), device="cuda", dtype=bool)))
