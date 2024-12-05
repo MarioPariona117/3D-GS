f9d995c255f99cc812edd8c13dccbeaadb153f0f
elacs <keeyunshao@gmail.com>
2024-12-05 15:09:58 +0000

    try

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -628,6 +628,10 @@ class GaussianModel:
         
         return new_newly_cloned """
         #print(newsize)
+        togrow.backward(torch.ones_like(togrow))
+        print(self.growth_directions_probabilities.grad)
+        print(self.growth_length_s.grad)
+
         togrow_sum = torch.sum(togrow, dim = 1)
         togrow_x_sum, togrow_y_sum, togrow_z_sum = togrow_sum[0], togrow_sum[1], togrow_sum[2]
 
