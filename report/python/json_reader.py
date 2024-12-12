9925f46af614abbb1b92229131256eded04e5342
Mario Pariona Molocho <mariopariona117@gmail.com>
2024-12-12 18:58:14 +0000

    .

--- report/python/json_reader.py
+++ report/python/json_reader.py
@@ -11,10 +11,10 @@ print("Current working directory:", os.getcwd())
 #     "LLFF": ["horns", "trex"]
 # }
 DIRECTORY = {
-    "MipNeRF360 Outdoor": ["bicycle", "stump"],
-    "MipNeRF360 Indoor": ["counter"],
-    "Tanks \& Temples": ["truck", "train"],
-    "Deep Blending": ["playroom"],
+    # "MipNeRF360 Outdoor": ["bicycle", "stump"],
+    "MipNeRF360 Indoor": ["counter", "kitchen", "bonsai", "room"],
+    "Tanks \& Temples": ["train"],
+    "Deep Blending": [],
     "LLFF": ["horns", "trex"]
 }
 # SCENES = ["bycicle", "stump", "counter", "playroom", "truck", "train", "horns", "trex"]
@@ -28,12 +28,11 @@ for scenes in DIRECTORY.values():
         comparison += f"""
             \\includegraphics[width=0.26\\textwidth]{{../o-3dgs/eval/{scene}/test/ours_30000/gt/00000.png}} &
             \\includegraphics[width=0.26\\textwidth]{{../o-3dgs/eval/{scene}/test/ours_30000/renders/00000.png}} & 
-            \\includegraphics[width=0.26\\textwidth]{{../o-3dgs/eval/{scene}/test/ours_30000/renders/00000.png}} \\\\"""
+            \\includegraphics[width=0.26\\textwidth]{{../eval/{scene}/test/ours_30000/renders/00000.png}} \\\\"""
 
 comparison += """
     \\end{tabular}
     """
-
 with open("report/aux/comparison.tex", "w") as file:
     file.write(comparison)
 
@@ -50,13 +49,17 @@ for cat, scenes in DIRECTORY.items():
     \\multirow{{{len(scenes) * 2}}}{{*}}{{\\textbf{{{cat}}}}} & """
     for scene in scenes:
         o_3d_gs_metrics = json.load(open(f"o-3dgs/eval/{scene}/results.json"))["ours_30000"]
-        our_metrics = json.load(open(f"o-3dgs/eval/{scene}/results.json"))["ours_30000"]
-        # our_metrics = json.load(open(f"eval/{scene}/results.json"))["ours_30000"]
         metrics += f"""
         \\multirow{{2}}{{*}}{{{scene.capitalize()}}} 
         & 3D-GS & {o_3d_gs_metrics["SSIM"]:.3f} & {o_3d_gs_metrics["PSNR"]:.2f} & {o_3d_gs_metrics["LPIPS"]:.3f} & {int(o_3d_gs_metrics["Memory"])}MB \\\\"""
-        metrics += f"""
-        & & \\textbf{{Our Model}} & {our_metrics["SSIM"]:.3f} & {our_metrics["PSNR"]:.2f} & {our_metrics["LPIPS"]:.3f} & {int(our_metrics["Memory"])}MB \\\\"""
+        try:
+            our_metrics = json.load(open(f"eval/{scene}/results.json"))["ours_30000"]
+            metrics += f"""
+            & & \\textbf{{Our Model}} & {our_metrics["SSIM"]:.3f} & {our_metrics["PSNR"]:.2f} & {our_metrics["LPIPS"]:.3f} & {int(our_metrics["Memory"])}MB \\\\"""
+        except Exception as e:
+            print(e)
+            metrics += f"""
+            & & 3D-GS & {o_3d_gs_metrics["SSIM"]:.3f} & {o_3d_gs_metrics["PSNR"]:.2f} & {o_3d_gs_metrics["LPIPS"]:.3f} & {int(o_3d_gs_metrics["Memory"])}MB \\\\"""
         if scene != scenes[-1]:
             metrics += """
             \\cmidrule{2-7} &"""
@@ -66,6 +69,5 @@ for cat, scenes in DIRECTORY.items():
 metrics += """
     \\bottomrule
     \\end{tabular}"""
-
 with open("report/aux/metrics.tex", "w") as file:
     file.write(metrics)
